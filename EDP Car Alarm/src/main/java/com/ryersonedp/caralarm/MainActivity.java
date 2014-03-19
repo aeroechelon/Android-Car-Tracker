140fd7c7d0abd779d598e7b7447e50b869e410e2
Marvin Bernal <bernal.marvin@gmail.com>
2014-03-18 21:49:07 -0400

    Updated MainActivity to interface with Parse.

--- EDP Car Alarm/src/main/java/com/ryersonedp/caralarm/MainActivity.java
+++ EDP Car Alarm/src/main/java/com/ryersonedp/caralarm/MainActivity.java
@@ -1,24 +1,22 @@
 package com.ryersonedp.caralarm;
 
+import android.app.Activity;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.ParseObject;
 import com.ryersonedp.caralarm.util.SystemUiHider;
-
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.location.Location;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -26,10 +24,11 @@ import android.view.View;
  *
  * @see SystemUiHider
  */
-public class MainActivity extends Activity implements GooglePlayServicesClient.OnConnectionFailedListener, GooglePlayServicesClient.ConnectionCallbacks{
+public class MainActivity extends Activity implements GooglePlayServicesClient.OnConnectionFailedListener, GooglePlayServicesClient.ConnectionCallbacks, GoogleMap.CancelableCallback{
 
 
     private static final String TAG = "MainActivity.java";
+
     private static final float CITY_ZOOM = 10.0f;
     private static final float REGION_ZOOM = 15.0f;
     private static final float STREET_ZOOM = 18.0f;
@@ -37,48 +36,27 @@ public class MainActivity extends Activity implements GooglePlayServicesClient.O
     private static final float RYERSON_LAT = 43.657689f;
     private static final float RYERSON_LNG = -79.378233f;
 
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = false;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 10000;
-
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
-    private static final boolean TOGGLE_ON_CLICK = true;
-
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
-    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
-    private SystemUiHider mSystemUiHider;
+    private static final int CAMERA_ANIMATE_DURATION_SLOW = 2000;
 
     private GoogleMap map;
     private Location location;
     private LocationClient locationClient;
 
+
     @Override
     public void onConnected(Bundle bundle) {
 
-        Log.wtf(TAG, "GooglePlayServices onConnected invoked()");
+        LatLng currentLatitudeAndLongitude;
 
         // Get the last location from Google Play Services
         location = locationClient.getLastLocation();
+        currentLatitudeAndLongitude = new LatLng(location.getLatitude(), location.getLongitude());
 
         // Animate the camera to zoom at the indicated position by LatLng
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), STREET_ZOOM));
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatitudeAndLongitude, STREET_ZOOM), CAMERA_ANIMATE_DURATION_SLOW, this);
+        map.addMarker(new MarkerOptions()
+                .position(currentLatitudeAndLongitude)
+                .title(getResources().getString(R.string.your_location)));
 
     }
 
@@ -92,141 +70,64 @@ public class MainActivity extends Activity implements GooglePlayServicesClient.O
 
     }
 
+    // Used when animation zoom is interrupted
+    @Override
+    public void onFinish() {
+        // do nothing on finish
+    }
+
+    @Override
+    public void onCancel() {
+        // do nothing on cancel
+    }
+
     public void onClickFindMyLocationButton(View view){
+
+        location = locationClient.getLastLocation();
+
         // Animate the camera to zoom at the indicated position by LatLng
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), STREET_ZOOM));
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), STREET_ZOOM), CAMERA_ANIMATE_DURATION_SLOW, this);
     }
 
     public void onClickFindCarLocationButton(View view){
 
+        LatLng ryersonLatitudeAndLongitude = new LatLng(RYERSON_LAT, RYERSON_LNG);
+
         // Animate the camera to zoom at the indicated position by LatLng
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(RYERSON_LAT, RYERSON_LNG), STREET_ZOOM));
-
-
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(ryersonLatitudeAndLongitude, STREET_ZOOM), CAMERA_ANIMATE_DURATION_SLOW, this);
+        map.addMarker(new MarkerOptions()
+                .position(ryersonLatitudeAndLongitude)
+                .title(getResources().getString(R.string.car_location)));
     }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
-        final View contentView = findViewById(R.id.fullscreen_content);
 
         // Retrieving an instance of Google Map
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.activity_main_google_map)).getMap();
-        map.setTrafficEnabled(true);
+
+        // Disables zoom in / out buttons at the bottom of the map
+        map.getUiSettings().setZoomControlsEnabled(false);
 
         // Initiating a locationClient reference using GooglePlayServicesClient
         locationClient= new LocationClient(this, this, this);
         locationClient.connect();
 
-        // Set up an instance of SystemUiHider to control the system UI for
-        // this activity.
-        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
-        mSystemUiHider.setup();
-        mSystemUiHider
-                .setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-                    // Cached values.
-                    int mControlsHeight;
-                    int mShortAnimTime;
+        // Initializing Parse
+        Parse.initialize(this, getResources().getString(R.string.parse_applicationID), getResources().getString(R.string.parse_clientID));
 
-                    @Override
-                    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-                    public void onVisibilityChange(boolean visible) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-                            // If the ViewPropertyAnimator API is available
-                            // (Honeycomb MR2 and later), use it to animate the
-                            // in-layout UI controls at the bottom of the
-                            // screen.
-                            if (mControlsHeight == 0) {
-//                                mControlsHeight = controlsView.getHeight();
-                            }
-                            if (mShortAnimTime == 0) {
-                                mShortAnimTime = getResources().getInteger(
-                                        android.R.integer.config_shortAnimTime);
-                            }
-//                            controlsView.animate()
-//                                    .translationY(visible ? 0 : mControlsHeight)
-//                                    .setDuration(mShortAnimTime);
-                        } else {
-                            // If the ViewPropertyAnimator APIs aren't
-                            // available, simply show or hide the in-layout UI
-                            // controls.
-//                            controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
-                        }
+        // Obtaining a Parse reference
+        ParseObject parse = new ParseObject("TestObject");
+        parse.put("foo", "bar");
+        parse.saveInBackground();
 
-                        if (visible && AUTO_HIDE) {
-                            // Schedule a hide().
-                            delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                        }
-                    }
-                });
-
-        // Set up the user interaction to manually show or hide the system UI.
-        contentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (TOGGLE_ON_CLICK) {
-                    mSystemUiHider.toggle();
-                } else {
-                    mSystemUiHider.show();
-                }
-            }
-        });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-//        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-    }
+        }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        if(AUTO_HIDE) {
-            delayedHide(100);
-        }
     }
-
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
-
-    Handler mHideHandler = new Handler();
-    Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            mSystemUiHider.hide();
-
-        }
-    };
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
-
-
 }
